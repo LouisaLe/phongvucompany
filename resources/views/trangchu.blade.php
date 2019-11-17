d78501f489736f7312b705d49a205f1e681fed78
Louisa Le <nhiltb3994@gmail.com>
2019-11-17 22:01:58 +0700

    FE common ui init

---
+++ resources/views/trangchu.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Phong Vu Fashion</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="css/app.css" rel="stylesheet">
+
+        <!-- Styles -->
+    </head>
+    <body>
+        @include('common.menu')
+        This is homepage!
+        <!-- Slider banner -->
+        <div class="slick-slider__wrapper">
+            <div class="slick-sider__item">
+                <img class="slick-slider__img">
+            </div> 
+        </div>
+
+        @include('common.footer')        
+        <script   src="https://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('trangchu');
+});
+
+
+Route::get('/gioithieu', function () {
+    return view('intro');
 });
