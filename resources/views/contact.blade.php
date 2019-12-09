f90a11454e8d22b79e3714adabe2e9601d5ce186
LouisaLe <30714566+LouisaLe@users.noreply.github.com>
2019-12-09 14:47:12 +0700

    FE update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -28,16 +28,15 @@
         <!-- Styles -->
     </head>
     <body>
-        <div class="header__wrapper"> 
-    <div class="logo">
-        <a href="#">
-            <img src="images/logo-phong-vu.png" alt="Phong Vu Company - Fashion for all">
-        </a>
+    <div class="header__wrapper">
+        <div class="logo">
+            <a href="#">
+                <img src="images/logo-phong-vu.png" alt="Phong Vu Company - Fashion for all">
+            </a>
+        </div>
         <p class="slogan">Fashion for all</p>
-    </div>
-
-    @include('common.menu')        
-</div>
+        @include('common.menu')        
+    </div>       
 
 <div class="content__container">
     <div class="contact__wrapper">
@@ -97,6 +96,7 @@
 </script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
-<script src="js/main.js"></script>
+<script src="access/slick.min.js"></script>
+<script src="js/main.js"></script> 
 
 </body>
--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -15,16 +15,15 @@
         <!-- Styles -->
     </head>
     <body>
-        <div class="header__wrapper">
-            <div class="logo">
-                <a href="#">
-                    <img src="images/logo-phong-vu.png" alt="Phong Vu Company - Fashion for all">
-                </a>
-                <p class="slogan">Fashion for all</p>
-            </div>
-
-            @include('common.menu')        
+    <div class="header__wrapper">
+        <div class="logo">
+            <a href="#">
+                <img src="images/logo-phong-vu.png" alt="Phong Vu Company - Fashion for all">
+            </a>
         </div>
+        <p class="slogan">Fashion for all</p>
+        @include('common.menu')        
+    </div>
         <!-- <div class="img__banner">
             <img src="images/img-01.png" alt="Phong Vu Company">
         </div> -->
@@ -47,6 +46,12 @@
             </div>
         </div>
         @include('common.footer')
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHqvk9n1UMCme4tmMCtTzz_0VUuO2Br5Y&callback=initMap">
+        </script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+        <script src="access/slick.min.js"></script>
+        <script src="js/main.js"></script>
     </body>
 </htm>
 
